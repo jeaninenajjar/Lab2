7caed3e8d1cb21438f2a697a4334f27c0291adac
Jeanine Najjar <jnajjar@udel.edu>
2015-09-23 15:52:45 -0400

    updated deck

--- PokerEngine/src/main/Deck.java
+++ PokerEngine/src/main/Deck.java
@@ -1,15 +1,33 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Deck {
+	public static void main(String[] args) {
 
 	/*	Each deck (you could have multiple decks, depending on the game) is an array of cards.
 	•	When you instantiate a deck, you should build the array list of cards.  You should also “shuffle” the deck (make the order of cards random).
 	•	A method is needed that  will be to “draw” from the deck (return next card… assuming deck is shuffled).
 	•	Build a method to denote how many cards are remaining to be drawn.
-
+ 
 	*/	
 	
 	
+	Integer[] deck1 = new Integer [52];
+	
+    ArrayList<Integer> deck = new ArrayList<>(Arrays.asList(deck1));
+
+	
+    java.util.Collections.shuffle(deck);
+
+	 
+	//the method draw
+	
+	Card c = card();
+	c = Deck.get(0);
+	Deck.remove(0);
+	return c;
 	
 	
 	
@@ -17,5 +35,8 @@ public class Deck {
 	
 	
 	
+	
+	
+	}
 }
 
